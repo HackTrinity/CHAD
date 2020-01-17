3415b98e0e4efb640b3b2e1910dac7a1816f92bb
Jack O'Sullivan <jackos1998@gmail.com>
2020-01-17 14:15:17 +0000

    server: Add debug support

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-exec gunicorn 'CHAD:app' \
-    --bind '[::]:80' \
-    --workers "$WORKERS" \
-    --worker-class gevent
+if [ -n "$DEBUG" ]; then
+    exec python -u -m CHAD
+else
+    exec gunicorn 'CHAD:app' \
+        --bind '[::]:80' \
+        --workers "$WORKERS" \
+        --worker-class gevent
+fi
